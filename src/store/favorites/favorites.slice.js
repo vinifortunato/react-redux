6902bef40a7f76cbc14cfdc6b9dd540e6cdc7b26
Vinicius Fortunato <vinicius.fortunato@grupoboticario.com>
2022-11-03 17:54:44 -0300

    Initial commit

---
+++ src/store/favorites/favorites.slice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const itemsSlice = createSlice({
+  name: 'favorites',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      const item = action.payload;
+      return [...state, item.id];
+    },
+    remove: (state, action) => {
+        const filtered = state.filter((id) => id !== action.payload.id);
+        return filtered;
+    }
+  }
+});
+
+export const actions = itemsSlice.actions;
+export const reducer = itemsSlice.reducer;
+
+export default itemsSlice;
